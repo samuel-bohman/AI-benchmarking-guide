917c8b6cce76abfbb5eced2f2ffb0327c56c37c0
gitaumark <156354973+gitaumark@users.noreply.github.com>
2025-02-10 13:25:10 -0500

    Update tools.py

--- Infra/tools.py
+++ Infra/tools.py
@@ -1,5 +1,6 @@
 import os
-
+import datetime
+pwd = os.getcwd() + "/Outputs/log.txt"
 
 def create_dir(name: str):
     current = os.getcwd()
@@ -7,5 +8,16 @@ def create_dir(name: str):
     isdir = os.path.isdir(outdir)
     if not isdir:
         os.mkdir(outdir)
-
     return outdir
+
+def write_log(message: str, filename: str = pwd):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_entry = f"[{timestamp}]\n {message}\n"
+
+    with open(filename, "a") as file:
+        file.write(log_entry)
+
+def check_error(results):
+    if results.stderr:
+        return results.stderr.decode("utf-8")
+    return results.stdout.decode("utf-8")
