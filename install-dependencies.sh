e6bbaa4a754eb79e6b67a652b07cbf0d37eec0da
Jesse Lopez <lopezjesse@microsoft.com>
2025-02-26 02:05:12 +0000

    Add actual support for ROCm/AMD in install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -17,12 +17,10 @@ usage() {
     exit 1
 }
 
-# Check if help was requested
 if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     usage
 fi
 
-# Assign defaults if no arguments are provided
 pip=${1:-pip3}
 platform=${2:-nvidia}
 
@@ -33,14 +31,41 @@ else
     echo "- Installing dependencies in virtual environment located at: ${VIRTUAL_ENV}"
 fi
 echo ""
-echo ""
 
-$pip install -r requirements_torch.txt
-$pip install $(cat requirements_flashattn.txt) --no-build-isolation
 if [[ "$platform" == "nvidia" ]]; then
+    $pip install -r requirements_torch_nvidia.txt
+    $pip install $(cat requirements_flashattn.txt) --no-build-isolation
     $pip install -r requirements_main.txt
 elif [[ "$platform" == "amd" ]]; then
-    # tensorrt breaks install on AMD SKUs
+    # Install PyTorch: https://pytorch.org/get-started/locally/
+    $pip install -r requirements_torch_amd.txt
+    # Still need other packages for AMD so adding them here so they are grouped similarly to Nvidia
+    # Can't add in requirements because the index-url doesn't have them available
+    $pip install packaging setuptools wheel
+
+    # Install steps to build FlashAttention: https://github.com/Dao-AILab/flash-attention
+    echo "Building FlashAttention for AMD/ROCm"
+    if [ -d triton ]; then
+        echo "triton already exists.  Skipping clone."
+    else
+        git clone https://github.com/triton-lang/triton
+    fi
+    pushd triton > /dev/null
+    git checkout 3ca2f498e98ed7249b82722587c511a5610e00c4
+    $pip install --verbose -e python
+    popd > /dev/null
+
+    export FLASH_ATTENTION_TRITON_AMD_ENABLE="TRUE"
+    if [ -d 'flash-attention' ]; then
+        echo "flash-attention already exists.  Skipping clone."
+    else
+        git clone https://github.com/Dao-AILab/flash-attention.git
+    fi
+    pushd flash-attention > /dev/null
+    python setup.py install
+    popd > /dev/null
+
+    # tensorrt won't install on AMD SKUs
     $pip install $(grep -v tensorrt requirements_main.txt)
 else
     echo "Specified target platform ${platform} not recognized. Supported platform 'nvidia' or 'amd'"
