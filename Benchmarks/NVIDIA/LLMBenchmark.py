ff56243c91420c4ce6c3d2e34fb5b3107f90c34c
gitaumark <156354973+gitaumark@users.noreply.github.com>
2025-03-26 15:23:17 -0400

    Update LLMBenchmark.py

--- Benchmarks/NVIDIA/LLMBenchmark.py
+++ Benchmarks/NVIDIA/LLMBenchmark.py
@@ -29,12 +29,16 @@ class LLMBenchmark:
     def install_requirements(self):
         # Install required packages
         print("Installing Required Packages")
-        i2 = subprocess.run("apt-get -y install libopenmpi-dev", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        i2 = subprocess.run("pip install --extra-index-url https://pypi.nvidia.com/ tensorrt-llm==0.15.0", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         tools.write_log(tools.check_error(i2))
-        i2 = subprocess.run("pip3 install --no-cache-dir --extra-index-url https://pypi.nvidia.com tensorrt-libs", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        i2 = subprocess.run("sudo apt update && sudo apt-get -y install libopenmpi-dev", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        tools.write_log(tools.check_error(i2))
+        i2 = subprocess.run("pip3 install --no-cache-dir --extra-index-url https://pypi.nvidia.com tensorrt-libs==8.6.1", shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         tools.write_log(tools.check_error(i2))
 
         os.environ['HF_HOME'] = self.dir_path
+        
+        # you can manually set this environment variable - just find out where python site-packages are located and find tensorrt_libs and tensorrt_llm
         os.environ['LD_LIBRARY_PATH'] = "/home/azureuser/.local/lib/python3.10/site-packages/tensorrt_libs/:/home/azureuser/.local/lib/python3.10/site-packages/tensorrt_llm/libs" + os.environ['LD_LIBRARY_PATH']
 
         # Clone TensorRT-LLM repo
